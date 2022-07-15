f4beb36724fd9fcc819c0e84e6c31859cf11ae5b
lecky93 <zengqiang@163.com>
2022-07-15 14:36:25 +0800

    modify

--- configs/smallobject/uperattention_convnext_base_fp16_512x1024_320k_cityscape.py
+++ configs/smallobject/uperattention_convnext_base_fp16_512x1024_320k_cityscape.py
@@ -97,7 +97,7 @@ test_pipeline = [
     dict(
         type='MultiScaleFlipAug',
         img_scale=(2048, 1024),
-        img_ratios=[0.5, 0.75, 1.0, 1.25, 1.5, 1.75],
+        # img_ratios=[0.5, 0.75, 1.0, 1.25, 1.5, 1.75],
         flip=False,
         transforms=[
             dict(type='Resize', keep_ratio=True),
