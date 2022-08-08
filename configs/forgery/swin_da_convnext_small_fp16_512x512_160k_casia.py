ca69ed56628775994c45f1157f9b551d3dc79d1b
lecky93 <zengqiang@163.com>
2022-08-08 15:12:26 +0800

    modify

---
+++ configs/forgery/swin_da_convnext_small_fp16_512x512_160k_casia.py
@@ -0,0 +1,86 @@
+_base_ = [
+    '../_base_/datasets/casia.py',
+    '../_base_/default_runtime.py', '../_base_/schedules/schedule_160k.py'
+]
+
+crop_size = (512, 512)
+
+norm_cfg = dict(type='SyncBN', requires_grad=True)
+custom_imports = dict(imports='mmcls.models', allow_failed_imports=False)
+checkpoint_file = 'https://download.openmmlab.com/mmclassification/v0/convnext/downstream/convnext-base_3rdparty_32xb128-noema_in1k_20220301-2a0ee547.pth'  # noqa
+model = dict(
+    type='EncoderDecoder',
+    pretrained=None,
+    backbone=dict(
+        type='mmcls.ConvNeXt',
+        arch='base',
+        out_indices=[0, 1, 2, 3],
+        drop_path_rate=0.4,
+        layer_scale_init_value=1.0,
+        gap_before_final_norm=False,
+        init_cfg=dict(
+            type='Pretrained', checkpoint=checkpoint_file,
+            prefix='backbone.')),
+    decode_head=dict(
+        type='Swin_DAHead',
+        in_channels=[96, 192, 384, 768],
+        in_index=[0, 1, 2, 3],
+        channels=384,
+        depths=[2, 2, 18, 2],
+        num_heads=[3, 6, 12, 24],
+        dropout_ratio=0.1,
+        num_classes=2,
+        norm_cfg=norm_cfg,
+        align_corners=False,
+        loss_decode=dict(
+            type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0)),
+    # model training and testing settings
+    train_cfg=dict(),
+    test_cfg=dict(mode='slide', crop_size=crop_size, stride=(341, 341)))
+
+optimizer = dict(
+    constructor='LearningRateDecayOptimizerConstructor',
+    _delete_=True,
+    type='AdamW',
+    lr=0.0001,
+    betas=(0.9, 0.999),
+    weight_decay=0.05,
+    paramwise_cfg={
+        'decay_rate': 0.9,
+        'decay_type': 'stage_wise',
+        'num_layers': 12
+    })
+
+lr_config = dict(
+    _delete_=True,
+    policy='poly',
+    warmup='linear',
+    warmup_iters=1500,
+    warmup_ratio=1e-6,
+    power=1.0,
+    min_lr=0.0,
+    by_epoch=False)
+
+
+data = dict(
+    samples_per_gpu=2,
+    workers_per_gpu=2,
+    # test=dict(
+    #     split='CASIA1/splicing.txt'
+    # )
+)
+# # fp16 settings
+# optimizer_config = dict(type='Fp16OptimizerHook', loss_scale='dynamic')
+# # fp16 placeholder
+# fp16 = dict()
+
+runner = dict(type='IterBasedRunner', max_iters=160000)
+checkpoint_config = dict(by_epoch=False, interval=8000)
+evaluation = dict(interval=8000, metric=['mIoU', 'mFscore'], pre_eval=True)
+
+log_config = dict(
+    interval=50,
+    hooks=[
+        dict(type='TextLoggerHook', by_epoch=False),
+        dict(type='TensorboardLoggerHook')
+    ])
