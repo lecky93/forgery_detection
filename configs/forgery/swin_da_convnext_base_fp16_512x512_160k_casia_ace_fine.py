8f4bbb0d6a9a404b874d815caaeadbce997fcc7f
lecky93 <zengqiang@163.com>
2022-08-22 16:15:59 +0800

    modify

--- configs/forgery/swin_da_convnext_base_fp16_512x512_160k_casia_ace_fine.py
+++ configs/forgery/swin_da_convnext_base_fp16_512x512_160k_casia_ace_fine.py
@@ -75,9 +75,9 @@ optimizer_config = dict(type='Fp16OptimizerHook', loss_scale='dynamic')
 # fp16 placeholder
 fp16 = dict()
 
-runner = dict(type='IterBasedRunner', max_iters=320000)
-checkpoint_config = dict(by_epoch=False, interval=16000)
-evaluation = dict(interval=16000, metric=['mIoU', 'mFscore'], pre_eval=True)
+runner = dict(type='IterBasedRunner', max_iters=80000)
+checkpoint_config = dict(by_epoch=False, interval=8000)
+evaluation = dict(interval=8000, metric=['mIoU', 'mFscore'], pre_eval=True)
 
 log_config = dict(
     interval=50,
